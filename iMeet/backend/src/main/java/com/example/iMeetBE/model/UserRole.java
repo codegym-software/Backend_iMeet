9ef819e0109e35fb972a418171b43bef5c7c7132
VuHoHaiTrieu <157771273+VuHoHaiTrieu@users.noreply.github.com>
2025-09-29 14:53:09 +0700

    Delete iMeet/backend directory

--- iMeet/backend/src/main/java/com/example/iMeetBE/model/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.example.iMeetBE.model;
-
-public enum UserRole {
-    ADMIN,
-    STAFF,
-    USER
-}
--- iMeet/backend/src/main/resources/db/migration/V2__Remove_picture_url_column.sql
+++
@@ -1,2 +0,0 @@
--- Migration to remove picture_url column and use only avatar_url
-ALTER TABLE users DROP COLUMN picture_url;
