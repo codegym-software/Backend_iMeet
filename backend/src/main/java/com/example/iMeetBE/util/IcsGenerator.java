acbb80a83199556321278b8a2f8448512729a436
Tranngochung <bit230185@st.cmcu.edu.vn>
2025-12-10 10:25:16 +0700

    sửa file ics

--- backend/src/main/java/com/example/iMeetBE/util/IcsGenerator.java
+++ backend/src/main/java/com/example/iMeetBE/util/IcsGenerator.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.UUID;
 
 import com.example.iMeetBE.dto.MeetingResponse;
